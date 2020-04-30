820714f36109693d165a52882e75c8952c4374bf
Sam Creydt <screydt@gmail.com>
2020-04-30 19:53:20 -0400

    Small change

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ class App extends React.Component {
               }))
             }
           >
-            Toggle Lifecycles
+            Toggle Lifecycles (windows)
           </button>
           <button
             onClick={() =>
